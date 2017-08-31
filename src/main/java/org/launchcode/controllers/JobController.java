435e77b65853d39a7806bd95e65421c353505c26
Tri Tran <tritran20@gmail.com>
2017-08-30 19:57:24 -0700

    Added add job feature and form validation

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,11 +1,12 @@
 package org.launchcode.controllers;
 
-import org.launchcode.models.Job;
+import org.launchcode.models.*;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -50,18 +51,34 @@ public class JobController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
+    public String add(Model model, @ModelAttribute @Valid JobForm jobForm, Errors errors) {
 
         if (errors.hasErrors()) {
-            return "";
+
+            return "new-job";
+
         } else {
+
+            String name = jobForm.getName();
+            Employer employer = jobData.getEmployers().findById(jobForm.getEmployerId());
+            Location location = jobData.getLocations().findById(jobForm.getLocationId());
+            PositionType positionType = jobData.getPositionTypes().findById(jobForm.getPositionTypeId());
+            CoreCompetency coreCompetency = jobData.getCoreCompetencies().findById(jobForm.getCoreCompetencyId());
+
+            Job newJob = new Job(name, employer, location, positionType, coreCompetency);
+            jobData.add(newJob);
+
+            model.addAttribute("employer", employer);
+            model.addAttribute("name", name);
+            model.addAttribute("location", location);
+            model.addAttribute("positionType", positionType);
+            model.addAttribute("coreCompetency", coreCompetency);
 
             // TODO #6 - Validate the JobForm model, and if valid, create a
             // new Job and add it to the jobData data store. Then
             // redirect to the job detail view for the new Job.
 
-
-            return "";
+            return "job-detail";
 
         }
 
